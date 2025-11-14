50ac80b9b0a4d463d5c51d395f122d4877fe9884
Artem Jarovoj <artem.jarovoj17@googlemail.com>
2025-11-14 23:40:51 +0100

    deactivate crossorigin for testing aws reachability

--- src/main/java/de/jarovart/freemoment/server/controller/LocationController.java
+++ src/main/java/de/jarovart/freemoment/server/controller/LocationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/locations")
-@CrossOrigin(origins = "http://localhost:3000") // wichtig für React
+//@CrossOrigin(origins = "http://localhost:3000") // wichtig für React
 public class LocationController {
     
     @Autowired
